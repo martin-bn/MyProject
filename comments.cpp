3d2e1583ef88003cd0d23be1036d275cbe8b2984
Martin Nesse <mbnesse@gmail.com>
2020-07-29 14:25:45 +0200

    updateing

---
+++ comments.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    // Comments !!
+    cout << "Comments are fun";
+    /*
+     tekst tekst tekst tekst tekst 
+    */
+
+    return 0;
+}
+
